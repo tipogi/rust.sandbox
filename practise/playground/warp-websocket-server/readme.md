e9a7565f976524c86ac2d791f12cf3747cf9fef5
tipogi <tipogi@pm.me>
2023-04-17 17:21:30 +0200

    Warp web socket

---
+++ practise/playground/warp-websocket-server/readme.md
@@ -0,0 +1,5 @@
+# Warp WebSocket
+
+<p align="center"><img src="./static/wireframe.png" alt="integration cover"></p>
+
+Create a simple web socket server that can broadcast messages to the connected clients
---
+++ practise/playground/warp-websocket-server/src/main.rs
@@ -0,0 +1,97 @@
+// Add to use WebSocket::split
+use futures::StreamExt;
+use std::fs;
+use std::{env, net::SocketAddr};
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::{RwLock, mpsc};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use warp::ws::{Message, WebSocket};
+// use map function 
+use warp::Filter;
+
+static NEXT_USERID: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(1);
+
+type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
+
+#[tokio::main]
+async fn main() {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    println!("Address: {}", addr);
+
+    let socket_address: SocketAddr = addr.parse().expect("Cannot find a valid socket address");
+
+    let users = Users::default();
+    let users = warp::any().map(move || users.clone());
+
+    let hello_opt = warp::path::param::<String>()
+        .map(Some)
+        .or_else(|_| async { Ok::<(Option<String>,), std::convert::Infallible>((None,)) });
+
+    // PATH: /hello/xxxx
+    let hello_endpoint = warp::path("hello")
+        .and(hello_opt)
+        .and(warp::path::end())
+        .map(|name: Option<String>| {
+            format!("Hello, {}!", name.unwrap_or_else(|| "world".to_string()))
+        });
+
+    let ws_endpoint = warp::path("ws")
+        .and(warp::ws())
+        .and(users)
+        .map(| ws: warp::ws::Ws, users | ws.on_upgrade(move | socket | connect(socket, users)));
+
+    let files = warp::fs::dir("./static");
+
+    let res_404 = warp::any().map(|| {
+        warp::http::Response::builder()
+            .status(warp::http::StatusCode::NOT_FOUND)
+            .body(fs::read_to_string("./static/404.html").expect("404 404?"))
+    });
+
+    let routes = ws_endpoint.or(hello_endpoint).or(files).or(res_404);
+
+    let server = warp::serve(routes).try_bind(socket_address);
+
+    println!("Running server at {}!", addr);
+
+    server.await
+}
+
+
+async fn connect(ws: WebSocket, users: Users) {
+    let new_id = NEXT_USERID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+    println!("User with ID: {} connected!", new_id);
+    
+    // Split the ws transmit and recieve streams
+    let (user_tx, mut user_rx) = ws.split();
+    // Create a tunnel to talk with all the sockets
+    let (tx, rx) = mpsc::unbounded_channel();
+
+    let rx = UnboundedReceiverStream::new(rx);
+
+    tokio::spawn(rx.forward(user_tx));
+    users.write().await.insert(new_id, tx);
+
+    // Reading and broadcasting messages
+    while let Some(result) = user_rx.next().await {
+        println!("Message received");
+        broadcast_msg(result.expect("Failed to fetch message"), &users).await;
+    }
+
+    disconnect(new_id, &users).await;
+}
+
+async fn broadcast_msg(msg: Message, users: &Users) {
+    if let Ok(_) = msg.to_str() {
+        for (&_uid, tx) in users.read().await.iter() {
+            tx.send(Ok(msg.clone())).expect("Failed to send message");
+        }
+    }
+}
+
+async fn disconnect(user_id: usize, users: &Users) {
+    println!("User with ID {} disconnected", user_id);
+    users.write().await.remove(&user_id);
+}
